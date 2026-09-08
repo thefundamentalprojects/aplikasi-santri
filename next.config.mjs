96205a0b251aacfe9ab6982e1e3017a0d1cb9b29
iqbalsadewo <iqbalsad3wa@gmail.com>
2026-09-08 23:17:56 +0700

    feat: enable static export for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
